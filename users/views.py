9e18f963b6561f58159ae564ff0d0014d36adfa5
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-08 13:53:45 +0200

    Added view for users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
