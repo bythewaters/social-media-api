912c84bc2992ac31b94b6b6a235c48094727880b
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-09 15:42:34 +0200

    Added ManagerUserView for users app

--- users/views.py
+++ users/views.py
@@ -4,3 +4,10 @@ from users.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
