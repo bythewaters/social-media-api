c82d15cec895431a42ae99560ab3d779292311dd
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-09 15:41:58 +0200

    Added view for profile app

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, permissions, status
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from profiles.models import Profile
+from profiles.serializers import (
+    ProfileSerializer,
+    ProfileUpdateSerializer,
+)
+
+
+class ProfileListViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+class MyProfileViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = ProfileUpdateSerializer
+    queryset = Profile.objects.all()
+
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user)
+
+    @action(
+        methods=["GET", "PATCH"],
+        detail=False,
+        url_path="update",
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    def update_profile(self, request: Request) -> Response:
+        profile = self.request.user.profile
+        serializer = self.get_serializer(
+            profile, data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
