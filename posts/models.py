73a6487a195dd43319f442e35947027b26807ee4
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-12 15:34:41 +0200

    Added post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from social_media_api import settings
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="post"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.title}"
