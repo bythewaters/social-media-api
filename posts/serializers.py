2407daa43ad4c60ff5d4e6cfc7b4788d05ac0da5
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-12 15:36:09 +0200

    Added serializer for PostCreateView and RetrievePostView

---
+++ posts/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "owner",
+            "title",
+            "content",
+            "created_time",
+        ]
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "content",
+        ]
