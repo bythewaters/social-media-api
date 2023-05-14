49dee47358141f6f188f182f22fa152b836b0612
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-14 18:46:30 +0200

    Added view for create, list, update method

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import mixins, permissions
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from likes.models import Like
+from likes.serializers import LikeCreateSerializer, LikeSerializer, LikeDeleteSerializer
+
+
+class ReactionCreateViewSet(mixins.CreateModelMixin, GenericViewSet):
+    serializer_class = LikeCreateSerializer
+    queryset = Like.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ReactionDeleteViewSet(mixins.DestroyModelMixin, GenericViewSet):
+    serializer_class = LikeDeleteSerializer
+    queryset = Like.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ReactionListViewSet(mixins.ListModelMixin, GenericViewSet):
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
