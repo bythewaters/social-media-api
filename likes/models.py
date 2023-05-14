1fc5cac09658f2fc759e48b3fb210942c1c51995
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-14 18:45:34 +0200

    Added Like model in likes app

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from posts.models import Post
+from social_media_api import settings
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        related_name="likes",
+        on_delete=models.CASCADE,
+    )
